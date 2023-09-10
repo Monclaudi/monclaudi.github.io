b1787fc100c50c217f48bc958d3d4dfc1d144249
Gaurav <root@igauravsehrawat.com>
2023-09-10 23:04:51 +0200

    --add: about me and one cover song

---
+++ JS/script.js
@@ -0,0 +1,93 @@
+// Navbar and move to top button
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+  body.classList.add("disabledScroll");
+}
+
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  body.classList.remove("disabledScroll");
+}
+
+const siteLogo = document.querySelector('.logo a');
+
+// Get all the menu items
+const navLinks = document.querySelectorAll('.menu-list li');
+
+const playSongBtn = document.querySelector('.play-song-btn');
+
+// Add click event listener to each menu item
+navLinks.forEach(item => {
+  item.addEventListener('click', function () {
+    // Remove the 'active' class from all menu items
+    navLinks.forEach(menuItem => menuItem.classList.remove('active-menu-list'));
+
+    // Add the 'active' class to the clicked menu item
+    this.classList.add('active-menu-list');
+  });
+});
+
+playSongBtn.onclick = () => {
+  removeActiveClass();
+}
+
+siteLogo.onclick = () => {
+  removeActiveClass();
+};
+
+function removeActiveClass() {
+  navLinks.forEach(menuItem => menuItem.classList.remove('active-menu-list'));
+}
+
+window.onscroll = () => {
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+
+  // When the user scrolls down 75px from the top of the document, show the button
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    moveTopBtn.style.display = "block";
+  } else {
+    moveTopBtn.style.display = "none";
+  }
+}
+
+// PLAY MUSIC
+const playSong = document.getElementById("song");
+var icon = document.getElementById("icon");
+
+icon.addEventListener("click", playMusic);
+
+function playMusic() {
+  if (playSong.paused) {
+    playSong.play();
+    icon.src = "./assets/Image/pause.png";
+  } else {
+    playSong.pause();
+    icon.src = "./assets/Image/play.png";
+  }
+}
+
+// Move to Top button
+var moveTopBtn = document.getElementById("moveTopBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  removeActiveClass();
+}
+
+// PreLoader
+var loader = document.getElementById("loading");
+function preLoader() {
+  loader.style.display = "none";
+}
